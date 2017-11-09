21afa8f6a75e8049657f077789eeb24256474c87
Ignacio Casales <casales.ignacio@gmail.com>
2017-11-09 09:50:25 -0300

    Fixed models dependecy - Removed garbage files

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Cheaphotels
 
+> UTN - 2017
 
+Made by
+
+ - _Ignacio Casales_
+ - _Santiago Blanc_
+
+### Project Setup
+
+- Install **python 3.6.3**
+
+- Install **pip**
+
+- Install **Django 1.11.7** 
+```Bash
+$ pip install Django==1.11.7
+```
+
+- Install **pillow**
+
+````Bash
+$ pip install Pillow
+````
+
+- Migrate models
+````Bash
+$ manage.py migrate [appname]
+
+$ manage.py makemigrations [appname]
+````
--- bookings/models.py
+++ bookings/models.py
@@ -11,14 +11,6 @@ class Passenger(models.Model):
     email = models.EmailField(blank=False, null=False, max_length=200)
 
 
-class Reservation(models.Model):
-    reservation_date = models.DateField(blank=False, null=False, auto_now=True)
-    reservation_number = models.PositiveIntegerField(blank=False, null=False, unique=True)
-    total_amount = models.FloatField(blank=False, null=False)
-    passenger = models.OneToOneField(Passenger, on_delete=models.DO_NOTHING, blank=False, null=False)
-    property = models.ForeignKey(Property, on_delete=models.PROTECT, blank=False, null=False)
-
-
 class Property(models.Model):
     name = models.CharField(blank=False, null=False, max_length=50)
     description = models.TextField(blank=False, null=False, default="Awesome description")
@@ -27,6 +19,14 @@ class Property(models.Model):
     max_pax = models.PositiveIntegerField(blank=False, null=False)
     daily_cost = models.FloatField(blank=False, null=False)
     city = models.OneToOneField(City, on_delete=models.DO_NOTHING, blank=False, null=False)
+
+
+class Reservation(models.Model):
+    reservation_date = models.DateField(blank=False, null=False, auto_now=True)
+    reservation_number = models.PositiveIntegerField(blank=False, null=False, unique=True)
+    total_amount = models.FloatField(blank=False, null=False)
+    passenger = models.OneToOneField(Passenger, on_delete=models.DO_NOTHING, blank=False, null=False)
+    property = models.ForeignKey(Property, on_delete=models.PROTECT, blank=False, null=False)
 
 
 class ReservationDate(models.Model):
